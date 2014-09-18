43667d954af325411a54f107c3e3848f45719e70
Katlyn Schwaebe <schwaebek@gmail.com>
2014-09-18 15:45:15 -0400

    Katlyn's Reaction - leaderboard in Game Center

--- Katlyn's Reaction/ViewController.swift
+++ Katlyn's Reaction/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameKit
 
 let SCREEN_WIDTH = UIScreen.mainScreen().bounds.size.width
 let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.size.height
@@ -19,11 +20,14 @@ class ViewController: UIViewController {
     
     var scoreLabel = UILabel()
     var timer: NSTimer?
+    
     var currentScore = 0
     var buttonToTap = 0
     
+    var player = GKLocalPlayer.localPlayer()
     
-    
+    var allLeaderboards: [String:GKLeaderboard] = Dictionary()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -57,7 +61,7 @@ class ViewController: UIViewController {
         
         for i in 0..<buttons.count {
             var button = buttons[i]
-            var size: CGFloat = 100
+            var size: CGFloat = 100.0
             
             var x = (SCREEN_WIDTH / 2.0) - (size / 2.0)
             var y = (SCREEN_HEIGHT / 2.0) - (size / 2.0) - (CGFloat(i - 1) * (size + 20))
@@ -68,7 +72,7 @@ class ViewController: UIViewController {
             button.backgroundColor = UIColor.whiteColor()
             button.tag = i
             
-            button.addTarget(self, action: Selector("button:"), forControlEvents: .TouchUpInside)
+            button.addTarget(self, action: Selector("buttonTapped:"), forControlEvents: .TouchUpInside)
             
             self.view.addSubview(button)
             
@@ -83,6 +87,18 @@ class ViewController: UIViewController {
                 self.runLevel()
        
         }
+        var nc = NSNotificationCenter.defaultCenter()
+        nc.addObserver(self, selector: Selector("authChanged"), name: GKPlayerAuthenticationDidChangeNotificationName, object: nil)
+        
+            if player.authenticated == false {
+            player.authenticateHandler = { (viewController, error) -> Void in
+                
+                if viewController != nil{
+                    self.presentViewController(viewController, animated: true, completion: nil)
+                }
+            }
+            
+        }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -90,6 +106,7 @@ class ViewController: UIViewController {
         if timer != nil {timer!.invalidate()}
         
         timer = NSTimer.scheduledTimerWithTimeInterval(speed, target: self, selector: Selector("timerDone"), userInfo: nil, repeats: false)
+        timerBar.layer.removeAllAnimations()
         timerBar.frame.size.width = SCREEN_WIDTH
         
         
@@ -100,6 +117,7 @@ class ViewController: UIViewController {
     }
     
         func timerDone() {
+            submitScore()
             println("Game Over")
         }
         func buttonTapped(button: UIButton){
@@ -107,19 +125,68 @@ class ViewController: UIViewController {
             if buttonToTap == button.tag
             {
                 currentScore++
+                checkAchievement()
+                runLevel()
+                
             }else{
+                
+                submitScore()
+                currentScore = 0
                 println("Fail")
+                var time = dispatch_time(DISPATCH_TIME_NOW, Int64(3.0 * Double(NSEC_PER_SEC)))
+                
+                dispatch_after(time, dispatch_get_main_queue()) { () -> Void in
+                    self.runLevel()
+                    
+                
+                    
+                }
+               
             }
         }
         func runLevel() {
             buttonToTap = Int(arc4random_uniform(3))
             var button = buttons[buttonToTap]
             button.alpha = 1.0
-            UIView.animateWithDuration(0.4, animations: { () -> Void in
+            
+            UIView.animateWithDuration(1.0, delay: 0, options: .AllowUserInteraction, animations: { () -> Void in
                 button.alpha = 0.6
-            })
-            resetTimerWithSpeed(10)
+                }) { (succeeded: Bool) -> Void in
+            }
+               resetTimerWithSpeed(10)
         }
+    func authChanged(){
+        if player.authenticated == false { return }
+        GKLeaderboard.loadLeaderboardsWithCompletionHandler { (leaderboards, error) -> Void in
+            for leaderboard in leaderboards as [GKLeaderboard]{
+                var identifier = leaderboard.identifier
+                self.allLeaderboards[identifier] = leaderboard
+            }
+        }
+    }
+    func submitScore() {
+        var scoreReporter = GKScore(leaderboardIdentifier: "total_taps")
+        scoreReporter.value = Int64(currentScore)
+        scoreReporter.context = 0
+        
+        GKScore.reportScores([scoreReporter], withCompletionHandler: { (error) -> Void in
+            println("score reported")
+        })
+        
+        var player = GKPlayer()
+    }
+    func checkAchievement()
+    {
+        if currentScore >= 5 {
+            var score50 = GKAchievement(identifier: "score_50")
+            //GKAchievement(identifier: "score_50")
+            score50.percentComplete = 100.0
+            score50.showsCompletionBanner = true
+            GKAchievement.reportAchievements([score50], withCompletionHandler: { (error) -> Void in
+                println("achievement sent")
+            })
+        }
+    }
         
         override func didReceiveMemoryWarning() {
             super.didReceiveMemoryWarning()
